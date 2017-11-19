8939e4c760029bb272ae9b339bbddbb7f98ffb42
oleg <lolego1601@gmail.com>
2017-11-19 14:39:17 +0200

    add user mock service

---
+++ user-service/src/main/java/com/groupproject/userservice/mock/UserMockService.java
@@ -0,0 +1,14 @@
+package com.groupproject.userservice.mock;
+
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
+public class UserMockService {
+
+    @PostConstruct
+    public void mockDatabase() {
+
+    }
+}
